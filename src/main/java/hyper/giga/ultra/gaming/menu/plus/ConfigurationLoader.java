383aeb1f745e9441fdfc119db29f1c791dcdf94a
IAmMoltony <koronavirusnyj@gmail.com>
2025-10-03 16:59:47 +0500

    various configuration loader enhancements
    google hidamari sketch episode 5

--- src/main/java/hyper/giga/ultra/gaming/menu/plus/ConfigurationLoader.java
+++ src/main/java/hyper/giga/ultra/gaming/menu/plus/ConfigurationLoader.java
@@ -10,7 +10,10 @@ import hyper.giga.ultra.gaming.menu.plus.cool.CoolImage;
 import hyper.giga.ultra.gaming.menu.plus.cool.CoolImageBackground;
 import hyper.giga.ultra.gaming.menu.plus.cool.CoolImageBackgroundMode;
 import hyper.giga.ultra.gaming.menu.plus.cool.CoolSolidColorBackground;
+import hyper.giga.ultra.gaming.menu.plus.cool.CoolSound;
+import hyper.giga.ultra.gaming.menu.plus.menuitem.Alignment;
 import hyper.giga.ultra.gaming.menu.plus.menuitem.MenuItem;
+import hyper.giga.ultra.gaming.menu.plus.menuitem.SeparatorMenuItem;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
@@ -51,14 +54,20 @@ public class ConfigurationLoader
         return element == null ? defaultValue : element.getAsFloat();
     }
     
+    private static String jsonGetOrDefaultString(String property, JsonObject object, String defaultValue)
+    {
+        JsonElement element = jsonGetOrNull(property, object);
+        return element == null ? defaultValue : element.getAsString();
+    }
+    
     //
     // various thing parsers
     //
-    
-    private static int parseColorValue(JsonElement element)
+
+    private static int parseColorValueDefault(JsonElement element, int defaultValue)
     {
         if (element == null || element.isJsonNull() || !element.isJsonPrimitive()) {
-            throw new IllegalArgumentException("Color value not present or has incorrect type");
+            return defaultValue;
         }
         
         int value = element.getAsInt();
@@ -66,6 +75,15 @@ public class ConfigurationLoader
             throw new IllegalArgumentException("Color value must not be negative or higher than 255");
         }
 
+        return value;
+    }
+    
+    private static int parseColorValue(JsonElement element)
+    {
+        int value = parseColorValueDefault(element, -1);
+        if (value == -1) {
+            throw new IllegalArgumentException("Color value not present or has incorrect type");
+        }
         return value;
     }
 
@@ -85,6 +103,7 @@ public class ConfigurationLoader
         int r = parseColorValue(colorObject.get("r"));
         int g = parseColorValue(colorObject.get("g"));
         int b = parseColorValue(colorObject.get("b"));
+        
         return new Color(r, g, b);
     }
     
@@ -121,6 +140,9 @@ public class ConfigurationLoader
         return new CoolImage(imageFile, angle, scaleX, scaleY, tint);
     }
     
+    // this warning shows up when assigning the background mode
+    // spoiler alert: it is used
+    @SuppressWarnings("UnusedAssignment")
     private static CoolBackground parseBackground(JsonElement element, CoolBackground defaultValue)
     {
         if (element == null || element.isJsonNull()) {
@@ -183,23 +205,13 @@ public class ConfigurationLoader
                 CoolImageBackgroundMode mode = CoolImageBackgroundMode.Center;
                 if (!(modeStringObject == null || modeStringObject.isJsonNull() || !modeStringObject.isJsonPrimitive())) {
                     String modeString = modeStringObject.getAsString();
-                    switch (modeString) {
-                        case "center" -> {
-                            mode = CoolImageBackgroundMode.Center;
-                        }
-                        case "zoom" -> {
-                            mode = CoolImageBackgroundMode.Zoom;
-                        }
-                        case "tile" -> {
-                            mode = CoolImageBackgroundMode.Tile;
-                        }
-                        case "stretch" -> {
-                            mode = CoolImageBackgroundMode.Stretch;
-                        }
-                        default -> {
-                            throw new IllegalArgumentException(String.format("Unknown image background mode: %s", modeString));
-                        }
-                    }
+                    mode = switch (modeString) {
+                        case "center" -> CoolImageBackgroundMode.Center;
+                        case "zoom" -> CoolImageBackgroundMode.Zoom;
+                        case "tile" -> CoolImageBackgroundMode.Tile;
+                        case "stretch" -> CoolImageBackgroundMode.Stretch;
+                        default -> throw new IllegalArgumentException(String.format("Unknown image background mode: %s", modeString));
+                    };
                 }
                 Color backgroundColor = parseColor(backgroundObject.get("color"), Color.BLACK);
                 return new CoolImageBackground(image, mode, backgroundColor);
@@ -210,9 +222,105 @@ public class ConfigurationLoader
         }
     }
     
+    private static CoolBackground[] parseMenuItemBackgrounds(JsonObject itemObject)
+    {
+        JsonElement backgroundElement = itemObject.get("background");
+        if (backgroundElement == null || !backgroundElement.isJsonObject()) {
+            return new CoolBackground[] {
+                MenuItem.BACKGROUND_NORMAL_DEFAULT,
+                MenuItem.BACKGROUND_SELECTED_DEFAULT
+            };
+        }
+        
+        JsonObject backgroundObject = backgroundElement.getAsJsonObject();
+        return new CoolBackground[] {
+            parseBackground(backgroundObject.get("normal"), MenuItem.BACKGROUND_NORMAL_DEFAULT),
+            parseBackground(backgroundObject.get("selected"), MenuItem.BACKGROUND_SELECTED_DEFAULT)
+        };
+    }
+    
+    private static Alignment parseAlignment(JsonElement alignmentElement)
+    {
+        if (alignmentElement == null || !alignmentElement.isJsonPrimitive()) {
+            return Alignment.Center;
+        }
+        
+        String alignmentString = alignmentElement.getAsString();
+        
+        // this is what they call modern java i think
+        return switch (alignmentString) {
+            case "center" -> Alignment.Center;
+            case "left" -> Alignment.Left;
+            case "right" -> Alignment.Right;
+            default -> throw new IllegalArgumentException(String.format("Invalid alignment: %s", alignmentString));
+        };
+    }
+    
+    private static int[] parseOffset(JsonElement offsetElement)
+    {
+        if (offsetElement == null || !offsetElement.isJsonObject()) {
+            return new int[] {0, 0};
+        }
+        
+        JsonObject offsetObject = offsetElement.getAsJsonObject();
+        int x = jsonGetOrDefaultInt("x", offsetObject, 0);
+        int y = jsonGetOrDefaultInt("y", offsetObject, 0);
+        return new int[] {x, y};
+    }
+    
+    private static CoolSound[] parseSounds(JsonObject container)
+    {
+        JsonElement soundsElement = container.get("sounds");
+        if (soundsElement == null || !soundsElement.isJsonObject()) {
+            return new CoolSound[] {null, null};
+        }
+        JsonObject soundsObject = soundsElement.getAsJsonObject();
+        
+        String selectSoundPath = jsonGetOrDefaultString("select", soundsObject, null);
+        String interactSoundPath = jsonGetOrDefaultString("interact", soundsObject, null);
+        return new CoolSound[] {
+            selectSoundPath == null ? null : new CoolSound(selectSoundPath),
+            interactSoundPath == null ? null : new CoolSound(interactSoundPath)
+        };
+    }
+    
+    private static SeparatorMenuItem parseSeparator(JsonObject itemObject)
+    {
+        Color color = parseColor(itemObject.get("color"), Color.LIGHT_GRAY);
+        int thickness = jsonGetOrDefaultInt("thickness", itemObject, 1);
+        int length = jsonGetOrDefaultInt("length", itemObject, 80);
+        Alignment alignment = parseAlignment(itemObject.get("alignment"));
+        int[] offset = parseOffset(itemObject.get("offset"));
+        CoolBackground[] backgrounds = parseMenuItemBackgrounds(itemObject);
+        CoolSound[] sounds = parseSounds(itemObject);
+        return new SeparatorMenuItem(
+                backgrounds[0],
+                backgrounds[1],
+                sounds[0],
+                sounds[1],
+                color,
+                thickness,
+                length,
+                alignment,
+                offset[0],
+                offset[1]
+        );
+    }
+    
     private static MenuItem parseMenuItem(JsonObject itemObject)
     {
-        return null; // todo
+        JsonElement typeElement = itemObject.get("type");
+        if (typeElement == null || typeElement.isJsonNull() || !typeElement.isJsonPrimitive()) {
+            throw new IllegalArgumentException("Menu item type not present or invalid");
+        }
+        
+        String typeString = typeElement.getAsString();
+        switch (typeString) {
+            case "separator" -> {
+                return parseSeparator(itemObject);
+            }
+        }
+        throw new IllegalArgumentException(String.format("Invalid menu item type: %s", typeString));
     }
     
     private static Screen parseScreen(JsonObject screenObject) throws IllegalArgumentException
@@ -246,12 +354,15 @@ public class ConfigurationLoader
             }
         }
         
+        CoolSound[] sounds = parseSounds(screenObject);
+        
         return new Screen(
                 items,
                 width,
                 height,
                 background,
-                null, null
+                sounds[0],
+                sounds[1]
         );
     }
     
