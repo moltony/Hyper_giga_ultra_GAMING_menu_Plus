0ea2873ea84818b37e6f30cdb2f26168a0cf342b
IAmMoltony <koronavirusnyj@gmail.com>
2025-10-08 21:12:37 +0500

    half-competent gif decoder

--- src/main/java/hyper/giga/ultra/gaming/menu/plus/AnimationLoader.java
+++ src/main/java/hyper/giga/ultra/gaming/menu/plus/AnimationLoader.java
@@ -2,16 +2,32 @@ package hyper.giga.ultra.gaming.menu.plus;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import javax.imageio.ImageIO;
 import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.stream.ImageInputStream;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class AnimationLoader
 {
+    private static BufferedImage copyImage(BufferedImage source)
+    {
+        // https://stackoverflow.com/questions/3514158/how-do-you-clone-a-bufferedimage
+        ColorModel colorModel = source.getColorModel();
+        boolean isAlphaPremultiplied = source.isAlphaPremultiplied();
+        WritableRaster raster = source.copyData(null);
+        return new BufferedImage(colorModel, raster, isAlphaPremultiplied, null);
+    }
+    
+    // finally, a use for my PhD in graphics interchange format
     public static BufferedImage[] loadGIF(String path)
     {
         try {
@@ -38,16 +54,104 @@ public class AnimationLoader
             BufferedImage[] frameArray = frames.toArray(BufferedImage[]::new);
 
             // convert them to argb images
-            for (int i = 0; i < frameArray.length; i++) {
-                BufferedImage source = frameArray[i];
-                BufferedImage copy = new BufferedImage(source.getWidth(), source.getHeight(), BufferedImage.TYPE_INT_ARGB);
-                Graphics2D g = copy.createGraphics();
-                g.drawImage(source, 0, 0, null);
-                g.dispose();
-                frameArray[i] = copy;
-            }
             
-            return frameArray;
+            int maxWidth = 0, maxHeight = 0;
+            for (int i = 0; i < numFrames; i++) {
+                maxWidth = Math.max(reader.getWidth(i), maxWidth);
+                maxHeight = Math.max(reader.getHeight(i), maxHeight);
+            }
+            BufferedImage image = new BufferedImage(maxWidth, maxHeight, BufferedImage.TYPE_INT_ARGB);
+            Graphics2D g = image.createGraphics();
+            
+            BufferedImage[] converted = new BufferedImage[frameArray.length];
+            for (int i = 0; i < frameArray.length; i++) {
+                BufferedImage src = frameArray[i];
+
+                IIOMetadata meta = reader.getImageMetadata(i);
+                String formatName = meta.getNativeMetadataFormatName();
+                Node root = meta.getAsTree(formatName);
+                NodeList children = root.getChildNodes();
+                
+                // Graphics control extension
+                // TODO pretty sure it handles these automatically.
+                boolean transparentColorFlag = false;
+                int transparentColorIndex = 0;
+                
+                // Image descriptor
+                int imageLeftPosition = 0;
+                int imageTopPosition = 0;
+                int imageWidth = 0;
+                int imageHeight = 0;
+                
+                // Loop through metadata nodes. This is so Java I can't.
+                for (int j = 0; j < children.getLength(); j++) {
+                    Node node = children.item(j);
+                    if (node != null) {
+                        if (node.hasAttributes()) {
+                            NamedNodeMap map = node.getAttributes();
+                            for (int k = 0; k < map.getLength(); k++) {
+                                Node attribute = map.item(k);
+                                String attributeName = attribute.getNodeName();
+                                String attributeValue = attribute.getNodeValue();
+                                
+                                switch (node.getNodeName()) {
+                                    case "GraphicsControlExtension" -> {
+                                        switch (attributeName) {
+                                            // This thing controls how to dispose the frame
+                                            // See also: https://webreference.com.cach3.com/content/studio/disposal.html
+                                            case "disposalMethod" -> {
+                                                // TODO i don't have a gif with non-doNotDispose method tehepero
+                                            }
+                                            
+                                            // userInputFlag is supposed to be 'user interact with image, advance to next frame'
+                                            // but no one cared lmao
+                                            
+                                            // TODO handle delayTime
+                                            // Currently in CoolImage it's constant and, worst of all, set by the user.
+                                            // We don't trust the user btw.
+                                            
+                                            // Whether the frame has transparency.
+                                            case "transparentColorFlag" -> {
+                                                transparentColorFlag = "TRUE".equals(attributeValue);
+                                            }
+                                            
+                                            // Which color in the palette is transparent.
+                                            case "transparentColorIndex" -> {
+                                                transparentColorIndex = Integer.parseInt(attributeValue);
+                                            }
+                                        }
+                                    }
+                                    case "ImageDescriptor" -> {
+                                        switch (attributeName) {
+                                            case "imageLeftPosition" -> {
+                                                imageLeftPosition = Integer.parseInt(attributeValue);
+                                            }
+                                            case "imageTopPosition" -> {
+                                                imageTopPosition = Integer.parseInt(attributeValue);
+                                            }
+                                            case "imageWidth" -> {
+                                                imageWidth = Integer.parseInt(attributeValue);
+                                            }
+                                            case "imageHeight" -> {
+                                                imageHeight = Integer.parseInt(attributeValue);
+                                            }
+                                            
+                                            // interlaceFlag controls whether this frame is interlaced or not.
+                                            // IIO handles this so I don't even have to do anything.
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                
+                g.drawImage(src, imageLeftPosition, imageTopPosition, null);
+                converted[i] = copyImage(image);
+            }
+
+            g.dispose();
+            return converted;
         } catch (IOException exc) {
             ErrorHandler.handleError(exc, exc.toString());
             return null;
